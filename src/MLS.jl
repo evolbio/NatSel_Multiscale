6848ac1775a6586b7b1c084cd6cb5b8cdd8ac72d
Steve Frank <saf@stevefrank.org>
2024-09-11 14:51:30 -0700

    Working on dynamics with Beta distn and integration. Does not work well because Beta has singularity (infinity) at boundaries for some cases.

--- src/MLS.jl
+++ src/MLS.jl
@@ -1,5 +1,5 @@
 module MLS
-using DifferentialEquations
+using DifferentialEquations, Distributions, Integrals, Printf
 export discrete_dq, continuous_dq, qt
 
 s(m1, m2) = m1 - m2
@@ -25,6 +25,43 @@ end
 function continuous_dq(q0,m1,m2,T)
 	prob = ODEProblem((u,p,t) -> dq(u,m1,m2), q0, (0,T))
 	sol = solve(prob, Tsit5(), reltol = 1e-12, abstol = 1e-12)
+end
+
+function qbar_prime(qbar, m1, m2, s, r, k)
+	g = (1-r)/r
+	a = g*qbar
+	b = g*(1-qbar)
+	T = 1
+	prob = IntegralProblem((q,p) -> pdf(Beta(a,b),q)*wbar(q,m1,m2,T), (0,1))
+	soln = solve(prob, QuadGKJL())
+	ybar = soln.u
+	soln.resid > 1e-7 ? println("ybar resid = ", soln.resid) : nothing
+	prob = IntegralProblem((q,p) -> 
+		pdf(Beta(a,b),q)*discrete_dq(q,m1,m2,T,T)[2][2]*(k-wbar(q,m1,m2,T))^s,
+		(0,1))
+	soln = solve(prob, QuadGKJL())
+	soln.resid > 1e-7 ? println("qbar_prime resid = ", soln.resid) : nothing
+	return soln.u / (k-ybar)^s
+end
+
+############
+
+function qbar_prime_old(qbar, m1, m2, T, r)
+	g = (1-r)/r
+	a = g*qbar
+	b = g*(1-qbar)
+	# set k so that m* = s = 1/T
+	k = ℯ*(1-r+r/T)/(1-r)
+	prob = IntegralProblem((q,p) -> pdf(Beta(a,b),q)*wbar(q,m1,m2,T), (0,1))
+	soln = solve(prob, QuadGKJL())
+	ybar = soln.u
+	soln.resid > 1e-7 ? println("ybar resid = ", soln.resid) : nothing
+	prob = IntegralProblem((q,p) -> 
+			pdf(Beta(a,b),q)*discrete_dq(q,m1,m2,T,T)[2][2]*(k-wbar(q,m1,m2,T)),
+			(0,1))
+	soln = solve(prob, QuadGKJL())
+	soln.resid > 1e-7 ? println("qbar_prime resid = ", soln.resid) : nothing
+	return soln.u / (k-ybar)
 end
 
 end # module MLS
--- src/Run.jl
+++ src/Run.jl
@@ -12,3 +12,22 @@ plot(t,q)
 sol = continuous_dq(q0,m1,m2,T);
 plot!(sol)
 
+#############################
+
+q0 = 0.1;
+T = 1;
+m1 = 1/T;
+r = 0.1;
+
+out = [MLS.qbar_prime(q0,1/T,m1+d,T,r) - q0 for d in -0.1:0.01:0.1];
+
+#############################
+
+q0 = 0.1;
+s = 1;
+r = 0.1;
+k = 1;	# take k as e^t for period t of competition w/in groups, t = log k
+m1 = log(k*(1-r)/(1-r+r*s));
+
+out = [MLS.qbar_prime(q0,m1,m1+d,s,r,k) - q0 for d in -0.1:0.01:0.1];
+
